6dc9126b8aabc9be50922da8234acdd65be3924a
brsc2909 <brsc2909@users.noreply.github.com>
2022-09-11 18:53:02 +0000

    deploy: 2c7aaf36c65a3092527a7fb5c57c14a8ac76c84c

--- _next/static/BsB8j_w2wEFk6zO9DofdX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/389-ac8a432997d9f6f6.js","static/chunks/pages/index-cda8d45c720ead26.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
